0ecd2c8ff58914a79324358786377a5915537f2f
Оксана <ok.antonova@rambler.ru>
2022-05-03 19:55:10 +0300

    добавила хибернейт, БД

--- src/main/java/web/controller/WebController.java
+++ src/main/java/web/controller/WebController.java
@@ -1,13 +1,14 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/")
-    public String seyHello() {
+    @GetMapping(value = "/hello")
+    public String seyHello(Model model) {
         return "index";
     }
 }
